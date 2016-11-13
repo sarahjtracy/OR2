93d0f8065e0192fc58f08420e93bfaf09afaa5b7
sarahjtracy <sarahtracy14@gmail.com>
2016-11-13 15:45:12 -0500

    Removed player file

---
+++ createTeam.py
@@ -0,0 +1,3 @@
+def addToTeam(team, freeAgents):
+  for i in xrange(len(freeAgents)):
+
--- player.py
+++
@@ -1,8 +0,0 @@
-class player(object):
-    def __init__(self, name, position, per):
-        self.name = name
-        self.position = position
-        self.per = per
-
-    def __str__(self):
-        return self.name
