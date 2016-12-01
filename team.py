0d1a3603a4a28d864148e20c05295d3485162d0d
Narain Krishnamurthy <narainsk@cmu.edu>
2016-12-01 18:09:35 -0500

    team class

---
+++ team.py
@@ -0,0 +1,26 @@
+
+class team(object):
+
+   def __init__(self, budget, minPlayers, maxPlayers, minPlayerPerPos, maxPlayerPerPos):
+      self.budget = budget
+      self.minPlayers = minPlayers
+      self.maxPlayers = maxPlayers
+      self.minPlayerPerPos = minPlayerPerPos
+      self.maxPlayerPerPos = maxPlayerPerPos
+
+   def getBudget(self):
+      return self.budget
+
+   def getMinPlayers(self):
+      return self.getMinPlayers
+
+   def getMaxPlayers(self):
+      return self.getMaxPlayers
+
+   def getMinPlayerPos(self):
+      return self.getMinPlayerPos
+
+   def getMaxPlayerPos(self):
+      return self.getMaxPlayerPos
+
+
