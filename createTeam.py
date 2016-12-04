e3e86e8d988f9aa6d1e7062a301f05243326c560
sarahjtracy <sarahtracy14@gmail.com>
2016-12-04 12:18:49 -0500

    addToTeam returning player with max pts

--- createTeam.py
+++ createTeam.py
@@ -7,15 +7,17 @@ def addToTeam(teamId):
   n = len(roster)
   for i in xrange(n):
     playerIds += [roster[i]['PLAYER_ID']]
-  playerList = player.PlayerList().info()[0]
+  playerList = player.PlayerList().info()
   maxpts = 0
   maxpid = 0
-  for i in xrange(len(playerList)):
+  for i in xrange(len(playerList)-1):
     pid = playerList[i]['PERSON_ID']
-    pts = player.PlayerSummary(pid).headline_stats()['PTS']
+    pts = player.PlayerSummary(pid).headline_stats()[0]['PTS']
     if (pts > maxpts):
       maxpts = pts
       maxpid = pid
   maxplayer = player.PlayerSummary(maxpid).headline_stats()[0]['PLAYER_NAME'] 
+  print(str(maxplayer))
+  print(maxpts)
   
 addToTeam(1610612737)
